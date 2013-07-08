6c2f32b9d055345d513b5d6029998736de155708
逸才 <yicai.cyj@taobao.com>
2013-07-09 00:13:45 +0800

    Added lego and plain server
    Changed loads of stuff:
    - settings
    - component/utils
    - added lego and plain server

--- config/config.js
+++
@@ -1,1 +0,0 @@
-exports.PACKAGE_FIELDS = ['author', 'name', 'version']
--- test/test_publish.js
+++ test/test_publish.js
@@ -1,1 +1,10 @@
 "use strict";
+
+require('should')
+
+describe('publish', function() {
+
+  it('should publish mosaic', function(done) {
+    done()
+  })
+})
